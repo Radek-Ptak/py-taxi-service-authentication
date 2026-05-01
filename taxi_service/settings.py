06306dd9c1fd78247cca9480fb895ac91853c2ca
Radoslaw Ptak <radek1026@interi.pl>
2026-05-02 01:51:25 +0200

    Implement authentication and visit counter

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -129,5 +129,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/taxi/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
